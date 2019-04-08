0c1f052e46169b09d32b261b9b35ed98cd8d12d9
s-santoro <36934087+s-santoro@users.noreply.github.com>
2019-04-08 17:02:12 +0200

    removed unnecessary comment

--- classification/scripts/configs/Configurations.py
+++ classification/scripts/configs/Configurations.py
@@ -49,18 +49,3 @@ class Configurations:
             "titleRemoveStopWords": True,
         }
     ]
-
-
-'''
-TP: 296 FP: 0
-FN: 161 TN: 566
-              precision    recall  f1-score   support
-
-           0       0.78      1.00      0.88       566
-           1       1.00      0.65      0.79       457
-
-   micro avg       0.84      0.84      0.84      1023
-   macro avg       0.89      0.82      0.83      1023
-weighted avg       0.88      0.84      0.84      1023
-
-'''
