e1af30dcfb6f6a9aa8c00c93f84304c2bb740722
gian brunner <gianbrunner@gmail.com>
2019-05-28 16:47:43 +0200

    merged configs for rulebasedClassifier with configs for MLClassifier

--- classification/scripts/configs/Configurations.py
+++ classification/scripts/configs/Configurations.py
@@ -4,6 +4,9 @@
 class Configurations:
 
     configs = [
+        #
+        # Configurations for Rulebased Pipeline
+        #
         #### Classifier = Bag of Words Method ####
         {#0
             # text preprocessing
@@ -474,5 +477,200 @@ class Configurations:
             "whiteList": True,
             "keyAmount": 1,
             "keyValTreshold": 1
+        },
+        #
+        # Configurations for Machine-Learning Pipeline
+        #
+        {#13
+            # text preprocessing
+            "textToLowerCase": True,
+            "textReplaceUmlaut": True,
+            "textPriceTagger": True,
+            "textRemoveSpecialCharacters": True,
+            "textRemoveSingleCharacters": True,
+            "textRemoveMultiSpaces": True,
+            "textStemText": True,
+            "textRemoveStopWords": True,
+
+            # title preprocessing
+            "titleToLowerCase": True,
+            "titleReplaceUmlaut": True,
+            "titlePriceTagger": True,
+            "titleRemoveSpecialCharacters": True,
+            "titleRemoveSingleCharacters": True,
+            "titleRemoveMultiSpaces": True,
+            "titleStemText": True,
+            "titleRemoveStopWords": True,
+
+            # Parameters for Data-Visualization
+            "n_most_freq_words": 20,
+            "n_most_freq_words_per_class": 15,
+            "n_most_freq_bigrams_per_class": 15,
+
+            # train test split
+            "test_size": .3,
+            "shuffle": True,
+            #"stratify": y,
+            "random_state": 3,
+
+            # bag of words
+            "use_BoW": False,
+            #"maxFeatures": 50,
+            "maxFeatures": 200,
+
+            # tf-idf
+            "use_tfidf": True,
+            "ngram_range": (1, 2),
+            "min_df": 2,
+            "max_df": .95,
+
+            # dimension reduction (truncated svd)
+            "use_dimension_reduction": True,
+            "n_components": 100,
+            "n_iter": 10
+        },
+        {#14
+                   # text preprocessing
+            "textToLowerCase": True,
+            "textReplaceUmlaut": True,
+            "textPriceTagger": True,
+            "textRemoveSpecialCharacters": True,
+            "textRemoveSingleCharacters": True,
+            "textRemoveMultiSpaces": True,
+            "textStemText": True,
+            "textRemoveStopWords": True,
+
+            # title preprocessing
+            "titleToLowerCase": True,
+            "titleReplaceUmlaut": True,
+            "titlePriceTagger": True,
+            "titleRemoveSpecialCharacters": True,
+            "titleRemoveSingleCharacters": True,
+            "titleRemoveMultiSpaces": True,
+            "titleStemText": True,
+            "titleRemoveStopWords": True,
+
+            # Parameters for Data-Visualization
+            "n_most_freq_words": 20,
+            "n_most_freq_words_per_class": 15,
+            "n_most_freq_bigrams_per_class": 15,
+
+            # train test split
+            "test_size": .3,
+            "shuffle": True,
+            #"stratify": y,
+            "random_state": 3,
+
+            # bag of words
+            "use_BoW": True,
+            #"maxFeatures": 50,
+            "maxFeatures": 200,
+
+            # tf-idf
+            "use_tfidf": False,
+            "ngram_range": (1, 2),
+            "min_df": 2,
+            "max_df": .95,
+
+            # dimension reduction (truncated svd)
+            "use_dimension_reduction": False,
+            "n_components": 100,
+            "n_iter": 10
+        },
+        {#15
+            # text preprocessing
+            "textToLowerCase": True,
+            "textReplaceUmlaut": True,
+            "textPriceTagger": True,
+            "textRemoveSpecialCharacters": True,
+            "textRemoveSingleCharacters": True,
+            "textRemoveMultiSpaces": True,
+            "textStemText": True,
+            "textRemoveStopWords": True,
+
+            # title preprocessing
+            "titleToLowerCase": True,
+            "titleReplaceUmlaut": True,
+            "titlePriceTagger": True,
+            "titleRemoveSpecialCharacters": True,
+            "titleRemoveSingleCharacters": True,
+            "titleRemoveMultiSpaces": True,
+            "titleStemText": True,
+            "titleRemoveStopWords": True,
+
+            # Parameters for Data-Visualization
+            "n_most_freq_words": 20,
+            "n_most_freq_words_per_class": 15,
+            "n_most_freq_bigrams_per_class": 15,
+
+            # train test split
+            "test_size": .3,
+            "shuffle": True,
+            #"stratify": y,
+            "random_state": 3,
+
+            # bag of words
+            "use_BoW": True,
+            #"maxFeatures": 50,
+            "maxFeatures": 200,
+
+            # tf-idf
+            "use_tfidf": False,
+            "ngram_range": (1, 2),
+            "min_df": 2,
+            "max_df": .95,
+
+            # dimension reduction (truncated svd)
+            "use_dimension_reduction": True,
+            "n_components": 100,
+            "n_iter": 10
+        },
+        {#16
+            # text preprocessing
+            "textToLowerCase": True,
+            "textReplaceUmlaut": True,
+            "textPriceTagger": True,
+            "textRemoveSpecialCharacters": True,
+            "textRemoveSingleCharacters": True,
+            "textRemoveMultiSpaces": True,
+            "textStemText": True,
+            "textRemoveStopWords": True,
+
+            # title preprocessing
+            "titleToLowerCase": True,
+            "titleReplaceUmlaut": True,
+            "titlePriceTagger": True,
+            "titleRemoveSpecialCharacters": True,
+            "titleRemoveSingleCharacters": True,
+            "titleRemoveMultiSpaces": True,
+            "titleStemText": True,
+            "titleRemoveStopWords": True,
+
+            # Parameters for Data-Visualization
+            "n_most_freq_words": 20,
+            "n_most_freq_words_per_class": 15,
+            "n_most_freq_bigrams_per_class": 15,
+
+            # train test split
+            "test_size": .3,
+            "shuffle": True,
+            #"stratify": y,
+            "random_state": 3,
+
+            # bag of words
+            "use_BoW": False,
+            #"maxFeatures": 50,
+            "maxFeatures": 200,
+
+            # tf-idf
+            "use_tfidf": True,
+            "ngram_range": (1, 2),
+            "min_df": 2,
+            "max_df": .95,
+
+            # dimension reduction (truncated svd)
+            "use_dimension_reduction": False,
+            "n_components": 100,
+            "n_iter": 10
         }
     ]
