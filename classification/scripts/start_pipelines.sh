715d2a3514ded68b60bec3c439a8e2c56a19cf33
gian brunner <gianbrunner@gmail.com>
2019-06-04 08:59:24 +0200

    Added output to inform how many pipelines are started.

--- classification/scripts/start_pipelines.sh
+++ classification/scripts/start_pipelines.sh
@@ -8,6 +8,6 @@ cores=$(fgrep -c processor /proc/cpuinfo)
 # get length of configs-list from python-script
 amountPipes=$(($(python3 get_configsSize.py)-1))
 echo 
-echo "starting ${amountPipes} pipelines with pipeline-end: ${end} ..."
+echo "starting $((1+${amountPipes})) pipelines with pipeline-end: ${end} ..."
 echo 
 time parallel -j${cores} ./pipeline.sh $end ::: $(seq 0 $amountPipes)
