e37ffcf0ec3906068879627094b2d91f8d021bfe
Gian Brunner <gian.brunner@gmail.com>
2019-07-02 15:11:07 +0200

    changed RBClassifier for dynamic and static output

--- classification/scripts/RulebasedClassifier.py
+++ classification/scripts/RulebasedClassifier.py
@@ -111,50 +111,73 @@ class RulebasedClassifier(Task):
             return self.runPricedetector(document)
 
     def runBoWRules(self, data):
-        # Split Data into Train Files (for generating BoW) and Test Files
+        output_df = pd.DataFrame(columns=('specified', 'predicted', 'url'))
+        # Decision to use Text or Title
         if Configurations().configs[self.configId].get("useText"):
-            dataParameter = "cleaned_text"
+                dataParameter = "cleaned_text"
         else:
             dataParameter = "title"
-        from sklearn.model_selection import train_test_split
-        trainData, testData, trainLabels, testLabels = train_test_split(data[dataParameter].values, data['class'].values,
-                                                                        test_size=Configurations().configs[self.configId].get("testSizeSplit"),
-                                                                        random_state=0)
-        output_df = pd.DataFrame(columns=('specified', 'predicted', 'url'))
-        output_df['specified'] = testLabels
-        # Sort Train Data
-        posExamples = []
-        negExamples = []
-        for index in range(len(trainData)):
-            if trainLabels[index] == 1:
-                posExamples.append(trainData[index])
-            else:
-                negExamples.append(trainData[index])
-        # Create Bag of Words
-        posWords = self.BagOfWords(posExamples)
-        negWords = self.BagOfWords(negExamples)
-        # Filter for Words occuring in positive and negative Examples
-        for i in range(len(posWords)):
-            for j in range(len(negWords)):
-                if posWords[i] == negWords[j]:
-                    posWords[i] = 'remove'
-                    negWords[j] = 'remove'
-        while 'remove' in posWords: posWords.remove('remove')
-        while 'remove' in negWords: negWords.remove('remove')
-        # Classify
-        for index2 in range(len(testData)):
-            output_df['url'].iloc[index2]=data['url'].iloc[index2]
-            score = Configurations().configs[self.configId].get("decisionLimit")
-            for word in posWords:
-                if word in testData[index2]:
-                    score = score + 1
-            for word in negWords:
-                if word in testData[index2]:
-                    score = score - 1
-            if score > 0:
-                output_df['predicted'].iloc[index2] = 1
-            else:
-                output_df['predicted'].iloc[index2] = 0
+        # Check if Lists should be created dynamically or static
+        if(Configurations().configs[self.configId].get("dynamic")):
+            
+            from sklearn.model_selection import train_test_split
+            # Split Data into Train Files (for generating BoW) and Test Files
+            trainData, testData, trainLabels, testLabels = train_test_split(data[dataParameter].values, data['class'].values,
+                                                                            test_size=Configurations().configs[self.configId].get("testSizeSplit"),
+                                                                            random_state=0)
+            output_df['specified'] = testLabels
+            # Sort Train Data 
+            posExamples = []
+            negExamples = []
+            for index in range(len(trainData)):
+                if trainLabels[index] == 1:
+                    posExamples.append(trainData[index])
+                else:
+                    negExamples.append(trainData[index])
+            # Create Bag of Words
+            posWords = self.BagOfWords(posExamples)
+            negWords = self.BagOfWords(negExamples)
+            # Filter for Words occuring in positive and negative Examples
+            for i in range(len(posWords)):
+                for j in range(len(negWords)):
+                    if posWords[i] == negWords[j]:
+                        posWords[i] = 'remove'
+                        negWords[j] = 'remove'
+            while 'remove' in posWords: posWords.remove('remove')
+            while 'remove' in negWords: negWords.remove('remove')
+            # Classify
+            for index2 in range(len(testData)):
+                output_df['url'].iloc[index2]=data['url'].iloc[index2]
+                score = Configurations().configs[self.configId].get("decisionLimit")
+                for word in posWords:
+                    if word in testData[index2]:
+                        score = score + 1
+                for word in negWords:
+                    if word in testData[index2]:
+                        score = score - 1
+                if score > 0:
+                    output_df['predicted'].iloc[index2] = 1
+                else:
+                    output_df['predicted'].iloc[index2] = 0
+        else:
+            # Use Static word lists generated out of test data of Gold Standard
+            posWords = ['abholung', 'aglio', 'ai', 'aktivier', 'al', 'alkoholfrei', 'alkoholisch', 'alla', 'allerg', 'anana', 'antipasti', 'apero', 'arrabbiata', 'artikelnr', 'artikelnumm', 'artischock', 'au', 'aubergi', 'balsamico', 'basilikum', 'beilag', 'beleg', 'beschreibung', 'bewertung', 'blattsala', 'bleu', 'bolog', 'bra', 'bro', 'broccoli', 'bun', 'burg', 'butt', 'cak', 'calzo', 'capr', 'casa', 'champigno', 'chef', 'cherry', 'cherrytoma', 'chick', 'chili', 'chocola', 'cm', 'cocktail', 'cordo', 'crevett', 'curry', 'delivery', 'ei', 'eier', 'enthal', 'erdbeer', 'erstell', 'extra', 'fil', 'focaccia', 'formaggi', 'fri', 'fruch', 'full', 'funghi', 'gang', 'garnier', 'glac', 'glacé', 'gnocchi', 'gorgonzola', 'gramm', 'grand', 'grill', 'grillier', 'gru', 'gurk', 'hawaii', 'hinzufug', 'hit', 'honig', 'hot', 'imbiss', 'ingw', 'insalata', 'italia', 'italienisch', 'joghur', 'kalb', 'kalbfleisch', 'kaper', 'karott', 'kartoffel', 'kas', 'kass', 'kategorie', 'kinderpizza', 'klassik', 'knoblauch', 'knusprig', 'krau', 'krauterbutt', 'kurier', 'lach', 'lamm', 'lasag', 'lieferzei', 'mar', 'margherita', 'marinier', 'mascarpo', 'meeresfruch', 'meng', 'menukar', 'mini', 'misch', 'mouss', 'mozzarella', 'mus', 'mws', 'napoli', 'normal', 'nudel', 'oliv', 'olivenol', 'oregano', 'panier', 'panna', 'parma', 'parmaschink', 'parmesa', 'penn', 'peperoncini', 'peperoni', 'pesto', 'pilz', 'pizz', 'pizzakurier', 'pizzeria', 'pollo', 'pomm', 'porcini', 'portio', 'poul', 'pouletbru', 'pouletfleisch', 'prosciutto', 'quattro', 'rahmsauc', 'registrier', 'registrierung', 'riesencrevett', 'rind', 'rindfleisch', 'rindsfil', 'risotto', 'rohschink', 'rot', 'rucola', 'rustica', 'saiso', 'salami', 'sandwich', 'sardell', 'sauc', 'scharf', 'schink', 'schnitzel', 'schwei', 'skip', 'softdrink', 'sortier', 'spaghetti', 'spargel', 'speck', 'spezial', 'spina', 'spirituo', 'stagioni', 'steinpilz', 'stuck', 'supp', 'suss', 'tagliatell', 'te', 'teigwar', 'tell', 'thai', 'tho', 'thunfisch', 'tiramisu', 'toma', 'tomatensauc', 'tonno', 'tortellini', 'uberback', 'us', 'vanill', 'vega', 'vegetarisch', 'vegi', 'verd', 'versa', 'versandko', 'vorbehal', 'vorspei', 'with', 'wunschli', 'wunschpizza', 'wurz', 'zigarett', 'zitro', 'zuck', 'zusatzstoff', 'zuta', 'zwiebel', 'zzgl']
+            negWords = ['abend', 'abmeld', 'ag', 'aktivita', 'allgemei', 'alt', 'ambie', 'anfahr', 'anmeldung', 'anrei', 'april', 'arbei', 'archiv', 'at', 'atmosphar', 'augu', 'bad', 'basel', 'bearbei', 'befi', 'bekann', 'ber', 'berei', 'bereich', 'beso', 'betrieb', 'bild', 'bildergalerie', 'bistro', 'bleib', 'buch', 'burtstag', 'business', 'club', 'cookie', 'dami', 'datenschutzerklarung', 'datum', 'desig', 'detail', 'deutsch', 'dezemb', 'dien', 'dienstleistung', 'doppelzimm', 'download', 'dritt', 'druck', 'durf', 'eign', 'einzelzimm', 'end', 'entdeck', 'erfahr', 'erfolg', 'erklar', 'erleb', 'erlebni', 'erreich', 'fahr', 'feiertag', 'fes', 'folg', 'foto', 'frag', 'fruhstuck', 'fuhr', 'fuhrung', 'gastgeb', 'gastronomie', 'geh', 'grund', 'grupp', 'herz', 'herzlich', 'heu', 'hinwei', 'hochzei', 'hor', 'hotel', 'http', 'idee', 'impressio', 'individuell', 'inter', 'it', 'jeweil', 'juli', 'juni', 'junior', 'kauf', 'klick', 'komm', 'kontaktformular', 'kontaktier', 'kostenlo', 'kulinarisch', 'kultur', 'kund', 'kur', 'lad', 'lagepla', 'land', 'lauf', 'leb', 'les', 'letz', 'lieb', 'lieg', 'lokal', 'loung', 'luzer', 'manag', 'map', 'marz', 'media', 'miet', 'mitarbei', 'mitglied', 'moch', 'moder', 'moglichkei', 'mona', 'musik', 'mutlich', 'nachrich', 'nah', 'neb', 'nehm', 'obe', 'of', 'offentlich', 'oktob', 'organisatio', 'ort', 'packag', 'party', 'pass', 'perfek', 'philosophie', 'pla', 'platz', 'press', 'priva', 'programm', 'raum', 'raumlichkei', 'rech', 'rechtlich', 'regio', 'reservierung', 'richtlinie', 'ruhetag', 'saal', 'sam', 'schenk', 'schutz', 'see', 'selb', 'sellschaf', 'seminar', 'seminarraum', 'send', 'setz', 'sitemap', 'somm', 'sond', 'spa', 'speziell', 'spiel', 'spor', 'sprach', 'st', 'stad', 'steh', 'ster', 'stund', 'sui', 'swiss', 'terrass', 'thi', 'threedigitpric', 'tischreservatio', 'tor', 'tour', 'ubernachtung', 'ubersich', 'unternehm', 'unterstutz', 'veranstaltung', 'verei', 'verfugung', 'verkauf', 'verw', 'video', 'voll', 'vornam', 'wand', 'web', 'webcam', 'webdesig', 'websei', 'wechsel', 'wel', 'welch', 'wett', 'win', 'wiss', 'woh', 'woll', 'you', 'zimm', 'zug', 'zusamm', 'zwisch']
+            output_df['specified'] = data['class'].values
+            # Classify      
+            for index in range(len(data[dataParameter].values)):
+                output_df['url'].iloc[index]=data['url'].iloc[index]
+                score = Configurations().configs[self.configId].get("decisionLimit")
+                for word in posWords:
+                    if word in data[dataParameter].iloc[index]:
+                        score = score + 1
+                for word in negWords:
+                    if word in data[dataParameter].iloc[index]:
+                        score = score - 1
+                if score > 0:
+                    output_df['predicted'].iloc[index] = 1
+                else:
+                    output_df['predicted'].iloc[index] = 0
         return output_df
 
     def BagOfWords(self, data):
