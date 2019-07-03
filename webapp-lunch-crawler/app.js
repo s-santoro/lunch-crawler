a806f0f00a0040b1d4cd562661eef3570c5d14f8
gian brunner <gianbrunner@gmail.com>
2019-07-03 14:17:19 +0200

    removed template-engine jade and replaced with severing static files: error.html and index.html

--- webapp-lunch-crawler/app.js
+++ webapp-lunch-crawler/app.js
@@ -5,13 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var lunchRouter = require('./routes/lunch');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/lunch', lunchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,9 +29,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // render error page
+  res.status(err.status || 500).sendFile(path.join(__dirname, 'views/error.html'));
 });
 
 module.exports = app;
--- webapp-lunch-crawler/routes/lunch.js
+++ webapp-lunch-crawler/routes/lunch.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET query results. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('get results from elastic');
 });
 
 module.exports = router;
