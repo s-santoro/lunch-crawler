b6a84f7d5fe0000e8bfe8f9f7b83f0c52458706a
Gian Brunner <gian.brunner@gmail.com>
2019-07-17 13:31:36 +0200

    changed path in random_script

--- gold-standard/generate_randomfiles.sh
+++ gold-standard/generate_randomfiles.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-ls Testdata_raw/Official_Testdata/Output/ | sort -R | head -500 | while read file; do
-	cp Testdata_raw/Official_Testdata/Output/$file files
+ls Output/ | sort -R | head -$1 | while read file; do
+	cp Output/$file files
 done
-
-
-
-#for (( i=1; i<=500; ++i))
-#do
-#	echo $i
-#done
