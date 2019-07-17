e54e44fc4b4b6f3a01dc84c0b39fbcd06d4ae406
Gian Brunner <gian.brunner@gmail.com>
2019-07-17 15:55:09 +0200

    new path defined

--- webapp-lunch-crawler/run_webapp/add_to_elasticSearch.sh
+++ webapp-lunch-crawler/run_webapp/add_to_elasticSearch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for filename in menu/*.json; do
+curl -XPUT http://localhost:9200/restaurants
+for filename in data_for_elasticsearch/out/*.json; do
 	curl -H 'Content-Type: application/json' -XPOST 'localhost:9200/restaurants/_doc' --data-binary @$filename
 done
